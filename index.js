34a32e037ae7c827cba69d38920efc2bc1e6963e
Zachary Youngdale <zyoungdale@gmail.com>
2022-08-29 13:54:44 -0400

    commit tree 14f6cf957401fe8b0eecf1ac75bd375db23c0c26

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 16
+let num4 = 4;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 / num4;
+
+let max = Math.max(1, 2, 3, 4, 20);
